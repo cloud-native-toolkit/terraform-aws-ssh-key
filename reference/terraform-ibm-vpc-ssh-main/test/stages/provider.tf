92576a2e267d62486fb65e34edce95b61e32dceb
sivasaivm <sivasaivm@in.ibm.com>
2022-02-03 16:06:17 +0530

    Update variables.tf
    Signed-off-by: sivasaivm <sivasaivm@in.ibm.com>

--- reference/terraform-ibm-vpc-ssh-main/test/stages/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "ibm" {
-  region           = var.region
-  ibmcloud_api_key = var.ibmcloud_api_key
-  ibmcloud_timeout = 900
-}
--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "secret_key" {
 
 variable "prefix_name" {
   type        = string
-  description = "Prefix to be added to the names of resources which are being provisioned"
+  description = "Prefix  to be added to the names of resources which are being provisioned"
   default     = "swe"
 }
 
