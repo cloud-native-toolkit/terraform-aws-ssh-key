7452f98314707c08b781a57e2ea7ff726de57c9a
sivasaivm <sivasaivm@in.ibm.com>
2022-02-03 14:26:24 +0530

    Update TF Variables
    Signed-off-by: sivasaivm <sivasaivm@in.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+REGION=$(cat terraform.tfvars | grep -E "^region" | sed "s/region=//g" | sed 's/"//g')
+
+aws configure set region ${REGION}
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+
+
 keyname="swe-prd"
 crefile=`ls -l *-key | wc -l`
 deffile=2
