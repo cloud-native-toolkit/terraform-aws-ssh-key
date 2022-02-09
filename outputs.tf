8768a7d728c0742acde0e0a39ac48e90dba7a8a0
sivasaivm <sivasaivm@in.ibm.com>
2022-02-09 19:20:22 +0530

    module.yaml update
    Signed-off-by: sivasaivm <sivasaivm@in.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,14 @@ output "usrprikey" {
   value     = tls_private_key.privatekey.private_key_pem
   sensitive = true
 }
+
 output "usrpubkey" {
   value = aws_key_pair.publickey.public_key
   sensitive = true
 }
+
+output "swesshkeyname" {
+value = data.aws_key_pair.swekey.key_name
+sensitive = true
+}
+
